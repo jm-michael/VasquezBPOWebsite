3ebae63e208e689a820083fa15c7516e7f51e48d
JMrevilla21 <jhonmichaelcarbon21@gmail.com>
2024-02-17 10:25:21 +0800

    move footers

--- script/index.js
+++ script/index.js
@@ -4,3 +4,41 @@ function toggleNav() {
     burger.classList.toggle('change');
     navLinks.classList.toggle('active');
 }
+
+// Get all the li elements
+var navLinks = document.querySelectorAll('.navbar-link');
+
+// Function to hide all iframes
+function hideAllIframes() {
+    var iframes = document.querySelectorAll('iframe');
+    for (var i = 0; i < iframes.length; i++) {
+        iframes[i].classList.add('hidden');
+    }
+}
+
+// Function to show iframe
+function showIframe(iframeSrc) {
+    var iframe = document.querySelector('iframe[src="' + iframeSrc + '"]');
+    if (iframe) {
+        iframe.classList.remove('hidden');
+    }
+}
+
+function removeSelected(){
+    for (var i = 0; i < navLinks.length; i++) {
+        navLinks[i].classList.remove('navbar-link__active');
+    }
+}
+
+// Add click event listener to each li
+for (var i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('click', function(e) {
+        e.preventDefault();
+        var iframeSrc = this.querySelector('a').getAttribute('href');
+        removeSelected();
+
+        e.currentTarget.classList.add('navbar-link__active');
+        hideAllIframes();
+        showIframe(iframeSrc);
+    });
+}
