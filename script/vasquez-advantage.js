903012315e1ff5caf301492deb223d597d520e7e
JMrevilla21 <jhonmichaelcarbon21@gmail.com>
2024-08-19 11:26:27 +0800

    advantage fix 3page

--- script/vasquez-advantage.js
+++ script/vasquez-advantage.js
@@ -6,6 +6,23 @@ const videoPlayer = document.getElementById('videoPlayer');
 const videoSource = document.getElementById('videoSource');
 const iframee = document.getElementById('iframee');
 
+const videos = [
+    "img/vasquez-advantage/page 2/This is Life at Vasquez (2nd page).mp4",
+    "img/vasquez-advantage/page 2/What makes Vasquez an advantage (2nd page).mp4",
+    "img/vasquez-advantage/page 3/By fostering career advancement in a healthy working environment.mp4",
+    "img/vasquez-advantage/page 3/By prioritizing employee wellbeing.mp4",
+    "img/vasquez-advantage/page 3/By providing opportunities to work in the United States.mp4",
+    "img/vasquez-advantage/page 3/Through compassionate and mindful leadership.mp4",
+]
+
+const pictures = [
+    "img/vasquez-advantage/page 3/By fostering career advancement in a healthy working environment.JPG",
+    "img/vasquez-advantage/page 3/By fostering career advancement in a healthy working environment.JPG",
+    "img/vasquez-advantage/page 3/By fostering career advancement in a healthy working environment.JPG",
+    "img/vasquez-advantage/page 3/By fostering career advancement in a healthy working environment.JPG",
+    "img/vasquez-advantage/page 3/By fostering career advancement in a healthy working environment.JPG",
+]
+
 minimizeBtn.addEventListener('click', () => {
     videoContainer.style.top = 'auto';
     videoContainer.style.bottom = '10px';
@@ -32,9 +49,9 @@ closeBtn.addEventListener('click', () => {
     videoContainer.classList.add('hidden');
 });
 
-function isVideo(show,iframe,link){
+function isVideo(show,iframe,indx){
     if (show) {
-        changeVideo(link);
+        changeVideo(videos[indx]);
         videoContainer.classList.remove('hidden');
     } else {
         videoContainer.classList.add('hidden');
@@ -56,3 +73,42 @@ function changeVideo(videoPath) {
     videoPlayer.play(); // Optionally start playing the new video automatically
 }
 
+const radios = document.querySelectorAll('input[name="position"]');
+const leftBtn = document.getElementById('leftBtn');
+const rightBtn = document.getElementById('rightBtn');
+var p3Index = 2;
+
+// Function to find the currently checked radio button
+function getCheckedRadioIndex() {
+    for (let i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            return i;
+        }
+    }
+    return -1; // No radio button is checked
+}
+
+// Add event listener for the "Previous" button
+leftBtn.addEventListener('click', function() {
+    let currentIndex = getCheckedRadioIndex();
+    if (currentIndex > 0) {
+        radios[currentIndex].checked = false;
+        radios[currentIndex - 1].checked = true;
+        p3Index = currentIndex - 1
+    }
+});
+
+// Add event listener for the "Next" button
+rightBtn.addEventListener('click', function() {
+    let currentIndex = getCheckedRadioIndex();
+    if (currentIndex < radios.length - 1) {
+        radios[currentIndex].checked = false;
+        radios[currentIndex + 1].checked = true;
+        p3Index = currentIndex + 1
+    }
+});
+
+function playVideo(){
+    console.log(p3Index+2)
+    isVideo(true,false,(p3Index+2))
+}
